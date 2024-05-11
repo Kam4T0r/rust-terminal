e80a7d5cf811c1344a6b685395da629e10568399
Kam4T0r <158210191+Kam4T0r@users.noreply.github.com>
2024-05-11 19:13:47 +0200

    Add files via upload

--- target/debug/rust-terminal.d
+++ target/debug/rust-terminal.d
@@ -1,1 +1,1 @@
-C:\Users\Kam4Tor\Desktop\rust-terminal\target\debug\rust-terminal.exe: C:\Users\Kam4Tor\Desktop\rust-terminal\src\main.rs
+C:\Users\Kam4Tor\Desktop\waletynk\rust-terminal\target\debug\rust-terminal.exe: C:\Users\Kam4Tor\Desktop\waletynk\rust-terminal\src\main.rs
